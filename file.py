f3b9aace03733fb9aff7b4f60a70b1f276e9bf59
Zikil <39454589+Zikil@users.noreply.github.com>
2019-12-04 23:04:32 +0700

    Add files via upload

---
+++ file.py
@@ -0,0 +1,3 @@
+{"type":"message_new","object":{"id":43, "date":1492522323, "out":0,
+    "user_id":xxxxxxxx, "read_state":0, "title":" ... ", "body":"помощь"},
+    "group_id":xxxxxxxxxxx}
---
+++ settings.py
@@ -0,0 +1,2 @@
+token = '5bd9a17a7ae21499ad7665029b2437b3907078eac8c99944ffa649053388b75b17a29beb769ba498aa018'
+confirmation_token = '6a70fec7'
